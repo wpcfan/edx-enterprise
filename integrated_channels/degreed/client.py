e6c26fc693673ad16a46d2c2450ce79e5d8f2921
Uman Shahzad <uman@opencraft.com>
2017-11-01 00:03:41 +0500

    Finish writing v1 of Degreed API Client.

--- integrated_channels/degreed/client.py
+++ integrated_channels/degreed/client.py
@@ -17,57 +17,12 @@ class DegreedAPIClient(object):
     Client for connecting to Degreed.
 
     Specifically, this class supports obtaining access tokens and posting to the courses and
-     completion status endpoints.
+    completion status endpoints.
     """
 
+    CONTENT_PROVIDER_SCOPE = 'provider_content'
+    COMPLETION_PROVIDER_SCOPE = 'provider_completion'
     SESSION_TIMEOUT = 5
-
-    @staticmethod
-    def get_oauth_access_token(url_base, client_id, client_secret, company_id, user_id, user_type):
-        """ Retrieves OAuth 2.0 access token using the client credentials grant.
-
-        Args:
-            url_base (str): Oauth2 access token endpoint
-            client_id (str): client ID
-            client_secret (str): client secret
-            company_id (str): Degreed company ID
-            user_id (str): Degreed user ID
-            user_type (str): type of Degreed user (admin or user)
-
-        Returns:
-            tuple: Tuple containing access token string and expiration datetime.
-        Raises:
-            HTTPError: If we received a failure response code from Degreed.
-            RequestException: If an unexpected response format was received that we could not parse.
-        """
-        DegreedGlobalConfiguration = apps.get_model(  # pylint: disable=invalid-name
-            'degreed',
-            'DegreedGlobalConfiguration'
-        )
-        global_degreed_config = DegreedGlobalConfiguration.current()
-        url = url_base + global_degreed_config.oauth_api_path
-
-        response = requests.post(
-            url,
-            json={
-                'grant_type': 'client_credentials',
-                'scope': {
-                    'userId': user_id,
-                    'companyId': company_id,
-                    'userType': user_type,
-                    'resourceType': 'learning_public_api',
-                }
-            },
-            auth=(client_id, client_secret),
-            headers={'content-type': 'application/json'}
-        )
-
-        response.raise_for_status()
-        data = response.json()
-        try:
-            return data['access_token'], datetime.datetime.utcfromtimestamp(data['expires_in'] + int(time.time()))
-        except KeyError:
-            raise requests.RequestException(response=response)
 
     def __init__(self, enterprise_configuration):
         """
@@ -86,30 +41,10 @@ class DegreedAPIClient(object):
 
         self.global_degreed_config = apps.get_model('degreed', 'DegreedGlobalConfiguration').current()
         self.enterprise_configuration = enterprise_configuration
-        self._create_session()
+        self.session = None
+        self.expires_at = None
 
-    def _create_session(self):
-        """
-        Instantiate a new session object for use in connecting with Degreed
-        """
-        session = requests.Session()
-        session.timeout = self.SESSION_TIMEOUT
-
-        oauth_access_token, expires_at = DegreedAPIClient.get_oauth_access_token(
-            self.enterprise_configuration.degreed_base_url,
-            self.enterprise_configuration.key,
-            self.enterprise_configuration.secret,
-            self.enterprise_configuration.degreed_company_id,
-            self.enterprise_configuration.degreed_user_id,
-            self.enterprise_configuration.user_type
-        )
-
-        session.headers['Authorization'] = 'Bearer {}'.format(oauth_access_token)
-        session.headers['content-type'] = 'application/json'
-        self.session = session
-        self.expires_at = expires_at
-
-    def send_completion_status(self, degreed_user_id, payload):
+    def send_completion_status(self, payload):
         """
         Send a completion status payload to the Degreed OCN Completion Status endpoint
 
@@ -124,7 +59,7 @@ class DegreedAPIClient(object):
             HTTPError: if we received a failure response code from Degreed
         """
         url = self.enterprise_configuration.degreed_base_url + self.global_degreed_config.completion_status_api_path
-        return self._call_post_with_user_override(degreed_user_id, url, payload)
+        return self._post(url, payload, self.COMPLETION_PROVIDER_SCOPE)
 
     def send_course_import(self, payload):
         """
@@ -139,53 +74,85 @@ class DegreedAPIClient(object):
             HTTPError: if we received a failure response code from Degreed
         """
         url = self.enterprise_configuration.degreed_base_url + self.global_degreed_config.course_api_path
-        return self._call_post_with_session(url, payload)
+        return self._post(url, payload, self.CONTENT_PROVIDER_SCOPE)
 
-    def _call_post_with_user_override(self, degreed_user_id, url, payload):
-        """
-        Make a post request with an auth token acquired for a specific user to a Degreed endpoint.
-
-        Args:
-            degreed_user_id (str): The user to use to retrieve an auth token.
-            url (str): The url to post to.
-            payload (str): The json encoded payload to post.
-        """
-        DegreedEnterpriseCustomerConfiguration = apps.get_model(  # pylint: disable=invalid-name
-            'degreed',
-            'DegreedEnterpriseCustomerConfiguration'
-        )
-        oauth_access_token, _ = DegreedAPIClient.get_oauth_access_token(
-            self.enterprise_configuration.degreed_base_url,
-            self.enterprise_configuration.key,
-            self.enterprise_configuration.secret,
-            self.enterprise_configuration.degreed_company_id,
-            degreed_user_id,
-            DegreedEnterpriseCustomerConfiguration.USER_TYPE_USER
-        )
-
-        response = requests.post(
-            url,
-            data=payload,
-            headers={
-                'Authorization': 'Bearer {}'.format(oauth_access_token),
-                'content-type': 'application/json'
-            }
-        )
-
-        return response.status_code, response.text
-
-    def _call_post_with_session(self, url, payload):
+    def _post(self, url, data, scope):
         """
         Make a post request using the session object to a Degreed endpoint.
 
         Args:
             url (str): The url to post to.
-            payload (str): The json encoded payload to post.
+            data (str): The json encoded payload to post.
+            scope (str): Must be one of
         """
         now = datetime.datetime.utcnow()
         if now >= self.expires_at:
             # Create a new session with a valid token
-            self.session.close()
-            self._create_session()
-        response = self.session.post(url, data=payload)
+            if self.session:
+                self.session.close()
+            self._create_session(scope=scope)
+        response = self.session.post(url, data=data)
         return response.status_code, response.text
+
+    def _create_session(self, scope):
+        """
+        Instantiate a new session object for use in connecting with Degreed
+        """
+        oauth_access_token, expires_at = self._get_oauth_access_token(
+            self.enterprise_configuration.degreed_base_url,
+            self.enterprise_configuration.key,
+            self.enterprise_configuration.secret,
+            self.enterprise_configuration.degreed_user_id,
+            self.enterprise_configuration.degreed_user_password,
+            scope
+        )
+        session = requests.Session()
+        session.timeout = self.SESSION_TIMEOUT
+        session.headers['Authorization'] = 'Bearer {}'.format(oauth_access_token)
+        session.headers['content-type'] = 'application/json'
+        self.session = session
+        self.expires_at = expires_at
+
+    def _get_oauth_access_token(self, url_base, client_id, client_secret, user_id, user_password, scope):
+        """ Retrieves OAuth 2.0 access token using the client credentials grant.
+
+        Args:
+            url_base (str): Oauth2 access token endpoint
+            client_id (str): client ID
+            client_secret (str): client secret
+            company_id (str): Degreed company ID
+            user_id (str): Degreed user ID
+            user_type (str): type of Degreed user (admin or user)
+
+        Returns:
+            tuple: Tuple containing access token string and expiration datetime.
+        Raises:
+            HTTPError: If we received a failure response code from Degreed.
+            RequestException: If an unexpected response format was received that we could not parse.
+            ValueError: If the provided scope does not match any available Degreed API provider scopes.
+        """
+        DegreedGlobalConfiguration = apps.get_model(  # pylint: disable=invalid-name
+            'degreed',
+            'DegreedGlobalConfiguration'
+        )
+        global_degreed_config = DegreedGlobalConfiguration.current()
+        url = url_base + global_degreed_config.oauth_api_path
+
+        response = requests.post(
+            url,
+            json={
+                'grant_type': 'password',
+                'username': user_id,
+                'password': user_password,
+                'scope': scope,
+            },
+            auth=(client_id, client_secret),
+            headers={'content-type': 'application/x-www-form-urlencoded'}
+        )
+
+        response.raise_for_status()
+        data = response.json()
+        try:
+            return data['access_token'], datetime.datetime.utcfromtimestamp(data['expires_in'] + int(time.time()))
+        except KeyError:
+            raise requests.RequestException(response=response)
