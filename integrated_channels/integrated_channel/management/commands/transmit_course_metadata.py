8de3dec45fc932b22f4dd072b6b180f236653dec
Uman Shahzad <uman@opencraft.com>
2017-11-05 00:40:20 +0500

    Make integrated channel management commands asynchronous.

--- integrated_channels/integrated_channel/management/commands/transmit_course_metadata.py
+++ integrated_channels/integrated_channel/management/commands/transmit_course_metadata.py
@@ -7,10 +7,11 @@ from __future__ import absolute_import, unicode_literals
 
 from logging import getLogger
 
+from integrated_channels.integrated_channel.management.commands import IntegratedChannelCommandMixin
+from integrated_channels.integrated_channel.tasks import send_data_task
+
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand, CommandError
-
-from . import INTEGRATED_CHANNEL_CHOICES, IntegratedChannelCommandMixin, celery_task
 
 LOGGER = getLogger(__name__)
 
@@ -51,32 +52,3 @@ class Command(IntegratedChannelCommandMixin, BaseCommand):
             channel_code = channel.channel_code()
             channel_pk = channel.pk
             send_data_task.delay(username, channel_code, channel_pk)
-
-
-@celery_task
-def send_data_task(username, channel_code, channel_pk):
-    """
-    Task to send course data to each linked integrated channel
-
-    Arguments:
-        channel_code (str): Capitalized identifier for the integrated channel
-        channel_pk (str): Primary key for identifying integrated channel
-
-    """
-    user = User.objects.get(username=username)
-    channel = INTEGRATED_CHANNEL_CHOICES[channel_code].objects.get(pk=channel_pk)
-
-    LOGGER.info('Processing courses for integrated channel using configuration: %s', channel)
-
-    try:
-        channel.transmit_course_data(user)
-    except Exception:  # pylint: disable=broad-except
-        exception_message = (
-            'Transmission of course metadata failed for user "{username}" and for integrated '
-            'channel with code "{channel_code}" and id "{channel_pk}".'.format(
-                username=username,
-                channel_code=channel_code,
-                channel_pk=channel_pk,
-            )
-        )
-        LOGGER.exception(exception_message)
---
+++ integrated_channels/integrated_channel/tasks.py
@@ -0,0 +1,52 @@
+"""
+Celery tasks for integrated channel management commands.
+"""
+
+from __future__ import absolute_import, unicode_literals
+
+from celery import task
+from celery.utils.log import get_task_logger
+from integrated_channels.integrated_channel.management.commands import INTEGRATED_CHANNEL_CHOICES
+
+from django.contrib.auth.models import User
+
+LOGGER = get_task_logger(__name__)
+
+
+@task(track_started=True)
+def send_data_task(username, channel_code, channel_pk):
+    """
+    Task to send course data to each linked integrated channel
+
+    Arguments:
+        channel_code (str): Capitalized identifier for the integrated channel
+        channel_pk (str): Primary key for identifying integrated channel
+
+    """
+    user = User.objects.get(username=username)
+    channel = INTEGRATED_CHANNEL_CHOICES[channel_code].objects.get(pk=channel_pk)
+
+    LOGGER.info('Processing courses for integrated channel using configuration: %s', channel)
+
+    try:
+        channel.transmit_course_data(user)
+    except Exception:  # pylint: disable=broad-except
+        exception_message = (
+            'Transmission of course metadata failed for user "{username}" and for integrated '
+            'channel with code "{channel_code}" and id "{channel_pk}".'.format(
+                username=username,
+                channel_code=channel_code,
+                channel_pk=channel_pk,
+            )
+        )
+        LOGGER.exception(exception_message)
+
+
+@task(track_started=True)
+def transmit_learner_data(username, channel_code, channel_pk):
+    """
+    Allows each enterprise customer's integrated channel to collect and transmit data within its own celery task.
+    """
+    api_user = User.objects.get(username=username)
+    integrated_channel = INTEGRATED_CHANNEL_CHOICES[channel_code].objects.get(pk=channel_pk)
+    integrated_channel.transmit_learner_data(api_user)
